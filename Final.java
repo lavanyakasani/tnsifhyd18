c437737824c0a864e02e6c1a49dacf0b1cb9ab02
lavanyakasani <168458371+lavanyakasani@users.noreply.github.com>
2024-05-19 08:58:14 +0530

    Final.java

---
+++ Final.java
@@ -0,0 +1,19 @@
+package com.coreconcepts.finall;
+
+public final class FinalBike {
+
+}
+public class FinalExtends extends FinalBike{
+	
+	void run() {
+		System.out.println("Running safely with 100kmph");
+	}
+	
+	public static void main(String[] args) {
+		
+		FinalExtends f1 = new FinalExtends();
+		f1.run();
+		
+	}
+
+}
