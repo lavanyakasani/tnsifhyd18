ac345d587c746d8f39a32e8f71885926233231d8
lavanyakasani <168458371+lavanyakasani@users.noreply.github.com>
2024-05-19 08:48:25 +0530

    Protected.java

---
+++ Protected.java
@@ -0,0 +1,19 @@
+package com.coreconcepts.accessmodifiers;
+
+public class ProtectedA {
+	
+	protected void display() {
+		System.out.println("TNS Sessions");
+	}
+
+}
+public class Protected2Main extends ProtectedA {
+	
+	public static void main(String[] args) {
+		
+		Protected2Main p3 = new Protected2Main();
+		p3.display();
+		
+	}
+
+}
