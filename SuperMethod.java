fa193fa14a73c2d5710c95d2d895a5ad7b6971cb
lavanyakasani <168458371+lavanyakasani@users.noreply.github.com>
2024-05-19 09:45:13 +0530

    SuperMethod.java

---
+++ SuperMethod.java
@@ -0,0 +1,26 @@
+package com.coreconcepts.superr;
+
+public class Animal2SuperParentMethod {
+	
+	void eat() {
+		System.out.println("eating...");
+	}
+
+}
+public class Animal2SuperChildMethod extends Animal2SuperParentMethod {
+	
+	void eat(){System.out.println("eating bread...");}  
+	void bark(){System.out.println("barking...");}  
+	void work(){  
+	super.eat();  
+	bark();  
+	eat();
+	}  
+	
+	public static void main(String[] args) {
+		Animal2SuperChildMethod a1 = new Animal2SuperChildMethod();
+		a1.work();
+	}
+
+
+}
