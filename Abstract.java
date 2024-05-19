2a4cf590602ddf48ff1eebf4623b6a30ef47222f
lavanyakasani <168458371+lavanyakasani@users.noreply.github.com>
2024-05-19 08:50:01 +0530

    Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,40 @@
+package com.coreconcepts.abstractclass;
+
+public class Veg extends Person{
+	
+	@Override
+	public void eat() {
+		System.out.println("Eats veg");
+	}
+
+}
+public class Nonveg extends Person {
+	
+
+	@Override
+	public void eat() {
+		System.out.println("Eats Nonveg");
+	}
+
+}
+public class Veg extends Person{
+	
+	@Override
+	public void eat() {
+		System.out.println("Eats veg");
+	}
+
+}
+public class MainAbstract {
+	
+	public static void main(String[] args) {
+		Person john = new Veg();
+		john.speak();
+		john.eat();
+		System.out.println("=======================");
+		Person mia = new Nonveg();
+		mia.speak();
+		mia.eat();
+	}
+
+}
